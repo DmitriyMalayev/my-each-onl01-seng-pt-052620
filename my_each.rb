0461c6d40c9e6c4c422cda2f1e98af17d2281606
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-06-04 20:44:12 -0400

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
+require "pry"
+
 def my_each(collection) # put argument(s) here
   i = 0 
   while i < collection.length 
     yield collection[i]
+    binding.pry 
     i += 1
   end
   collection  
